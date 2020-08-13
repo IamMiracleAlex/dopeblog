9d0b88ef728ea4b1f7751ad181929b43c70e2c7e
Miracle <collinsalex50@gmail.com>
2020-08-13 17:12:30 +0100

    changed db settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -61,11 +61,9 @@ if settings.DEBUG:
 
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'dopeblogdb',
-            'USER': 'postgres',
-            'PASSWORD': 'miracle',
-            'HOST': 'localhost'
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            
         }
     }
 
